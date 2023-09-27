91c244b0040dc8831fbb6b380acfdc61e94368a9
nyulan <yulan.nguyenvan@ynov.com>
2023-09-27 07:48:27 +0100

    zob

--- test.go
+++ test.go
@@ -54,13 +54,28 @@ type Equipement struct {
 	pied  Objet
 }
 
-func (p *Personnage) Init(nom string, class string, level int, pointVieMax int, pointVieActual int, coins int, inventory []Objet, skill []Sorts) {
+type Monstre struct {
+	nom                string
+	points_vie_maximum int
+	points_vie_actuels int
+	pointsAttaque      int
+}
+
+var m = Monstre{"Gobelin", 40, 40, 20}
+
+func (m *Monstre) InitGoblin(nom string, points_vie_maximum int, points_vie_actuels int, pointsAttaque int) {
+	m.nom = nom
+	m.points_vie_maximum = points_vie_maximum
+	m.points_vie_actuels = points_vie_actuels
+	m.pointsAttaque = pointsAttaque
+}
+
+func (p *Personnage) Init(nom string, class string, level int, pointVieMax int, pointVieActual int, coins int, skill []Sorts) {
 	p.nom = nom
 	p.class = class
 	p.level = level
 	p.pointVieMax = pointVieMax
 	p.pointVieActual = pointVieActual
-	p.inventory = inventory
 	p.coins = coins
 	p.skill = skill
 }
@@ -99,58 +114,64 @@ func (p *Personnage) RemoveObject(NomItem string) {
 	}
 }
 
-func (p *Personnage) limiteInventory() {
-	compteur := 0
-	for i := 0; i < len(p.inventory); i++ {
-		compteur += 1
-	}
-	if compteur >= 10 {
-		fmt.Println("Inventaire pleins ! ")
-	}
+func (p *Personnage) limitInventory() bool {
+	return len(p.inventory) < 10
 }
 
 func (p *Personnage) Inventory() {
 	for i := 0; i < len(p.inventory); i++ {
 		fmt.Println("["+strconv.Itoa(i+1)+"] ", p.inventory[i].nomItem, " : ", p.inventory[i].quantite)
+
 	}
-	fmt.Println("Quel equipement souhaitez vous ajouter  ? \n")
-	fmt.Println("1. Chapeau Magique")
-	fmt.Println("2. Tunique volante")
-	fmt.Println("3. Botte de sorcier")
-	fmt.Println("4. Retour")
-
+	fmt.Println("[" + strconv.Itoa(len(p.inventory)+1) + "] Retour")
 	choix, _ := Inputint()
+	if choix == len(p.inventory)+1 {
+		ClearConsole()
+		p.Menu()
+		// retour menu
+	} else {
+		var objet Objet = p.inventory[choix-1]
+		switch objet.nomItem {
+		case "Potions de vie":
+			p.takePot()
+			break
 
-	switch choix {
-	case 1:
-		if p.FindObject("Chapeau Magique") {
-			p.RemoveObject("Chapeau Magique")
-			p.Equipement.tete.nomItem = "Chapeau Magique" // Add stuff
-			p.pointVieMax += 10                           // Add PV
-			fmt.Println("Vous avez utilisé l'équipement Chapeau Magique")
-		} else {
-			fmt.Println("Vous ne posssedez pas cet élément")
+		case "Chapeau Magique":
+			if p.FindObject("Chapeau Magique") {
+				p.RemoveObject("Chapeau Magique")             // ICIIIIIIII
+				p.Equipement.tete.nomItem = "Chapeau Magique" // Add stuff
+				p.pointVieMax += 10
+				fmt.Println("Vous avez équipé le chapeau magique")
+				p.Inventory()
+
+			}
+			break
+
+		case "Tunique Volante":
+			if p.FindObject("Tunique Volante") {
+				p.RemoveObject("Tunique Volante")
+				p.Equipement.corps.nomItem = "Tunique Volante" // Add stuff
+				p.pointVieMax += 25
+				fmt.Println("Vous avez équipé la tunique Volante")
+				p.Inventory()
+			}
+			break
+
+		case "Bottes de sorcier":
+			if p.FindObject("Bottes de Sorcier") {
+				p.RemoveObject("Bottes de Sorcier")
+				p.Equipement.pied.nomItem = "Bottes de Sorcier" // Add stuff
+				p.pointVieMax += 15
+				fmt.Println("Vous avez equipé les Bottes de sorcier")
+				p.Inventory()
+			}
+			break
+
+		case "Retour ":
+			p.Menu()
+
 		}
 
-	case 2:
-		if p.FindObject("Tunique Volante") {
-			p.RemoveObject("Tunique Volante")
-			p.Equipement.corps.nomItem = "Tunique Volante" // Add stuff
-			p.pointVieMax += 25                            // Add PV
-			fmt.Println("Vous avez utilisé l'équipement Tunique volante")
-		} else {
-			fmt.Println("Vous ne posssedez pas cet élément")
-		}
-
-	case 3:
-		if p.FindObject("Botte de Sorcier") {
-			p.RemoveObject("Botte de Sorcier")
-			p.Equipement.pied.nomItem = "Botte de Sorcier" // Add stuff
-			p.pointVieMax += 15                            // Add PV
-			fmt.Println("Vous avez utilisé l'équipement Botte de Sorcier")
-		} else {
-			fmt.Println("Vous ne possedez pas cet élément")
-		}
 	}
 }
 
@@ -158,23 +179,28 @@ func (p *Personnage) Skill() {
 	fmt.Println(p.skill)
 }
 
-func (p *Personnage) dead() {
+func (p *Personnage) deadPersonnage() {
 	if p.pointVieActual < 0 {
 		fmt.Println("Vous etes mort !!!")
+		p.pointVieActual = p.pointVieActual / 2
+		fmt.Println("Vous etes résuscité avec la moitié de votre vie")
+		p.Menu()
+	}
+}
+func (m *Monstre) deadMonstre() {
+	if m.points_vie_actuels < 0 {
+		fmt.Println("Le monstre est mort !!!")
 	}
 }
 
-func (p *Personnage) spellBook() {
-}
-
 func (p *Personnage) takePot() {
-	fmt.Println("Vous avez utilisé la potion de vie")
 	if p.inventory[0].quantite > 0 {
 		p.inventory[0].quantite -= 1
 		if p.pointVieActual < 50 {
 			p.pointVieActual += 50
 		} else if p.pointVieActual >= 50 && p.pointVieActual < 100 {
 			p.pointVieActual = p.pointVieMax
+			fmt.Println("Vous avez utilisé la potion de vie")
 		} else {
 			fmt.Println("Impossible de boire la potion !")
 		}
@@ -184,16 +210,16 @@ func (p *Personnage) takePot() {
 
 }
 
-func (p *Personnage) poisonPot() {
-	vie := p.pointVieActual / 5 * 4
-	limitPv := p.pointVieActual - vie
-	for {
-		time.Sleep(2 * time.Second)
-		p.pointVieActual -= 5
-		if p.pointVieActual <= limitPv {
-			fmt.Println("Fin du poison")
-			break
+func (m *Monstre) poisonPot() {
+	for i := 0; i < 3; i++ {
+		time.Sleep(1 * time.Second)
+		if m.points_vie_actuels > 0 {
+			m.points_vie_actuels -= 5
+			if m.points_vie_actuels < 0 {
+				break
+			}
 		}
+		fmt.Println("[-10 POISON] Points de vie de l'ennemi : ", m.points_vie_actuels, "/", m.points_vie_maximum)
 	}
 }
 
@@ -205,12 +231,42 @@ func (p *Personnage) enoughMoney(prix int) bool {
 	}
 }
 
+func (m *Monstre) PlayerRound(p *Personnage) {
+	fmt.Println("1. Poison")
+	attaque, _ := Inputint()
+	switch attaque {
+	case 1:
+		fmt.Println("Vous avez utiliser le poison sur le monstre")
+		m.poisonPot()
+	}
+
+}
+
+func (p *Personnage) enemieRound() {
+	fmt.Println("Griffure")
+	p.pointVieActual -= m.pointsAttaque
+	fmt.Println("Vie restante", p.pointVieActual)
+}
+
+func (m *Monstre) trainFight(p *Personnage) {
+	for i := 0; p.pointVieActual > 0 || m.points_vie_actuels > 0; i++ {
+		m.PlayerRound(p)
+		m.deadMonstre()
+		p.enemieRound()
+		p.deadPersonnage()
+	}
+
+}
+
 func (p *Personnage) marchand() {
 	fmt.Println("Bienvenue chez le marchand que voulez vous acheter ? \n")
 	fmt.Println("1. Potion vie : 3 PO")
 	fmt.Println("2. Potion poison : 6 PO")
 	fmt.Println("3. Livre de Sort : Boule de feu : 25 PO")
-	fmt.Println("4. Retour")
+	fmt.Println("4. Chapeau Magique : 5 PO")
+	fmt.Println("5. Tunique Volante : 8 PO")
+	fmt.Println("6. Bottes de sorcier : 5 PO")
+	fmt.Println("7. Retour")
 	fmt.Printf("------------------ \n")
 	fmt.Printf("------------------ \n")
 	fmt.Printf("Votre solde actuel : %d\n", p.coins)
@@ -225,34 +281,48 @@ func (p *Personnage) marchand() {
 		prix := 3
 
 		if p.enoughMoney(prix) {
-			p.addInventory(nom, quantite, desc)
-			p.coins -= prix
-			ClearConsole()
-			fmt.Println("Vous avez acheté une potion de vie ")
-			p.marchand()
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté une potion de vie ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire !")
+				p.marchand()
+			}
 		} else {
 			ClearConsole()
 			fmt.Println("Vous n'avez pas assez d'argent ")
 			p.marchand()
 		}
-
+		break
 	case 2:
 		fmt.Println("Vous avez acheté une potion de poison ")
 		nom := "Potion de poison"
 		desc := "Enleve 20% des PV"
 		quantite := 1
 		prix := 6
+
 		if p.enoughMoney(prix) {
-			p.addInventory(nom, quantite, desc)
-			p.coins -= prix
-			ClearConsole()
-			fmt.Println("Vous avez acheté une potion de poison ")
-			p.marchand()
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté une potion de poison ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire ")
+				p.marchand()
+			}
 		} else {
 			ClearConsole()
 			fmt.Println("Vous n'avez pas assez d'argent ")
 			p.marchand()
 		}
+		break
 
 	case 3:
 		fmt.Println("Vous avez acheté une boule de feu ")
@@ -260,34 +330,113 @@ func (p *Personnage) marchand() {
 		desc := "Boule de feu qui enleve 25pv"
 		quantite := 1
 		prix := 25
+
 		if p.enoughMoney(prix) {
-			p.addInventory(nom, quantite, desc)
-			p.coins -= prix
-			ClearConsole()
-			fmt.Println("Vous avez acheté un livre de sort ")
-			p.marchand()
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté un livre de sort ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire ")
+				p.marchand()
+			}
 		} else {
 			ClearConsole()
 			fmt.Println("Vous n'avez pas assez d'argent ")
 			p.marchand()
 		}
+		break
 
 	case 4:
+		nom := "Chapeau Magique"
+		desc := ""
+		quantite := 1
+		prix := 5 // Add PV
+
+		if p.enoughMoney(prix) {
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté le chapeau magique ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire ")
+				p.marchand()
+			}
+		} else {
+			ClearConsole()
+			fmt.Println("Vous n'avez pas assez d'argent ")
+			p.marchand()
+		}
+		break
+	case 5:
+		nom := "Tunique Volante"
+		desc := ""
+		quantite := 1
+		prix := 8
+
+		if p.enoughMoney(prix) {
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté la tunique volante ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire ")
+				p.marchand()
+			}
+		} else {
+			ClearConsole()
+			fmt.Println("Vous n'avez pas assez d'argent ")
+			p.marchand()
+		}
+		break // Add PV
+
+	case 6:
+		// Add PV
+		nom := " Bottes de sorcier"
+		desc := ""
+		quantite := 1
+		prix := 5
+
+		if p.enoughMoney(prix) {
+			if p.limitInventory() {
+				p.addInventory(nom, quantite, desc)
+				p.coins -= prix
+				ClearConsole()
+				fmt.Println("Vous avez acheté les bottes de l'aventurier ")
+				p.marchand()
+			} else {
+				ClearConsole()
+				fmt.Println("Vous n'avez pas assez de place dans l'inventaire ")
+				p.marchand()
+			}
+		} else {
+			ClearConsole()
+			fmt.Println("Vous n'avez pas assez d'argent ")
+			p.marchand()
+
+		}
+		break
+	case 7:
 		ClearConsole()
 		p.Menu()
-
+		break
 	}
 }
 
 func main() {
 	var p1 Personnage
-	p1.Init("Harry", "Sorcier", 5, 100, 40, 100, []Objet{{"Potions de vie", "Donne de la vie", 0},
-		{"Chapeau Magique", "Donne de la vie", 1},
-		{"Tunique Volante", "Donne de la vie", 1},
-		{"Botte de Sorcier", "Donne de la vie", 1}}, []Sorts{{"Coup de poing", "Auto attaque de base", 10, 1}})
+	p1.Init("Harry", "Sorcier", 5, 100, 100, 100, []Sorts{{"Coup de poing", "Auto attaque de base", 10, 1}})
 	ClearConsole()
 	p1.Menu()
-
 }
 
 func (p *Personnage) addInventory(nom string, quantite int, desc string) {
@@ -338,6 +487,7 @@ func (p *Personnage) Menu() {
 	fmt.Printf("1.  Personnage \n")
 	fmt.Printf("2.  Inventaire \n")
 	fmt.Printf("3.  Marchand \n")
+	fmt.Printf("4.  Combat \n")
 
 	choice, _ := Inputint()
 
@@ -351,11 +501,13 @@ func (p *Personnage) Menu() {
 		fmt.Println("Affichage de l'inventaire :")
 		ClearConsole()
 		p.Inventory()
-		p.Menu()
 
 	case 3:
 		ClearConsole()
 		p.marchand()
+
+	case 4:
+		m.trainFight(p)
 
 	default:
 		fmt.Println("Choix invalide. Veuillez sélectionner une autre option.")
